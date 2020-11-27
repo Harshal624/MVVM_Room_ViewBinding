f51d03a55ef2ae9aae538d7d31d78c10c038087f
hArsh <123321pudaleharshal@gmail.com>
2020-11-27 19:55:16 +0530

    Latop model

---
+++ app/src/main/java/com/harsh/mvvmroom/Laptops.java
@@ -0,0 +1,69 @@
+package com.harsh.mvvmroom;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "laptop_table")
+public class Laptops {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String laptop_name;
+
+    private String laptop_description;
+
+    @ColumnInfo(name = "ram_size")
+    private int ram;
+
+    @ColumnInfo(name = "storage_size")
+    private int storage;
+
+    @ColumnInfo(name = "gpu_size")
+    private int gpu;
+
+    private long cost;
+
+    public Laptops(String laptop_name, String laptop_description, int ram, int storage, int gpu, long cost) {
+        this.laptop_name = laptop_name;
+        this.laptop_description = laptop_description;
+        this.ram = ram;
+        this.storage = storage;
+        this.gpu = gpu;
+        this.cost = cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    //room will use this method to set id for our table rows(autogenerate)
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLaptop_name() {
+        return laptop_name;
+    }
+
+    public String getLaptop_description() {
+        return laptop_description;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public int getStorage() {
+        return storage;
+    }
+
+    public int getGpu() {
+        return gpu;
+    }
+
+    public long getCost() {
+        return cost;
+    }
+}
